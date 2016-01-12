a33f7bd0da570361b0a34ddc51d93215f08e5ba2
b. binaohan <b@binaohan.org>
2016-01-11 19:06:39 -0500

    add readme, license, and change the name of the script

---
+++ README.md
@@ -0,0 +1,13 @@
+#Reallllly basic bash CLI for [Remember the Milk][milk]
+
+The only thing you'll need to install is [jq][jq], for working with JSON in bash. Of course, you can always figure out the XML stuff, but... It isn't like that's necessarily easier. Sort of a 'pick your poison' situation.
+
+You'll also need [your own API keys][api] because I haven't included mine here.
+
+I think other than [jq][jq], all other tools are the ones you generally find in a \*nix environment.
+
+Also noteworthy is that, yeah, I make pretty consistent use of the /tmp/ dir. Sue me, I find it easier and all the files are small and get cleaned out whenever you reboot. 
+
+[milk]: http://www.rememberthemilk.com
+[jq]: https://stedolan.github.io/jq/
+[api]: https://www.rememberthemilk.com/services/api/keys.rtm
--- rtm-cli.sh
+++ rtm-cli.sh
@@ -1,14 +1,24 @@
 #!/bin/bash
-
+#The first two lines of my config are:
+#api_key="your key here"
+#api_secret="your secret here"
+#You'll need this for all of the script.
 . ~/.rtmcfg
 
-#api_key api_secret
 api_url="https://api.rememberthemilk.com/services/rest/"
+#I find json easier to work with, but you can remove it
+#from here and in the $standard_args variable and you'll 
+#get xml back. For json you'll need to install jq, because 
+#this script relies heavily on it.
 format="json"
-standard_args="api_key=$api_key&format=$format&auth_token=$auth_token" #
+standard_args="api_key=$api_key&format=$format&auth_token=$auth_token"
+#You could easily swap this out for `curl -s` if you want.
+#I prefer wget for no principled reason.
 wget_cmd="wget -q -O -"
 
 #set options
+#i could do tags... and i do need to figure that out...
+#for now this will grab the list to add your task too.
 for i in "$@"
 do
 case $i in
@@ -19,50 +29,60 @@ case $i in
 esac
 done
 
-#sign requests
+#sign requests, pretty much all api calls need to be signed
+#https://www.rememberthemilk.com/services/api/authentication.rtm
 get_sig ()
 {
     echo -n $api_secret$(echo "$1" | tr '&' '\n' | sort | tr -d '\n' | tr -d '=') | md5sum | cut -d' ' -f1
 }
 
 #authorization
+#https://www.rememberthemilk.com/services/api/authentication.rtm
+#gets the frob and appends it to your .rtmcfg
 get_frob () {
     method="rtm.auth.getFrob"
     args="method=$method&$standard_args"
     sig=$(get_sig "$args")
     x=$($wget_cmd "$api_url?$args&api_sig=$sig" | jq -r '.rsp | .frob | @text')
-    echo "frob='$x'" >> .rtmcfg
+    echo "frob='$x'" >> ~/.rtmcfg
 }
-
+#builds the URL for giving permissison for the app to 
+#access your account. 
 auth_app () {
     auth_url="http://www.rememberthemilk.com/services/auth/"
     perms="delete"
     args="api_key=$api_key&perms=$perms&frob=$frob"
     sig=$(get_sig "$args")
-    surf "$auth_url?$args&api_sig=$sig"
+    x-www-browser "$auth_url?$args&api_sig=$sig"
 }
+#Once the window/tab/whatever is closed, this method is
+#called to get the all important auth_token. Which is
+#then appended to your .rtmcfg
 get_token () {
     method="rtm.auth.getToken"
     args="method=$method&$standard_args&frob=$frob"
     sig=$(get_sig "$args")
     token=$($wget_cmd "$api_url?$args&api_sig=$sig" | jq -r '.rsp | .auth | .token | @text')
-    echo "auth_token='$token'" >> .rtmcfg
+    echo "auth_token='$token'" >> ~/.rtmcfg
 }
 
+#bundles all the above steps
 authenticate () {
     get_frob
     . .rtmcfg
     auth_app
     get_token
 }
-
+#this is to check if your auth_token is valid
+#use this to troubleshoot if the authentication isn't working.
 check_token () {
     method="rtm.auth.checkToken"
     args="method=$method&$standard_args"
     sig=$(get_sig "$args")
     $wget_cmd "$api_url?$args&api_sig=$sig" | jq '.rsp | .stat'
 }
-
+#Grabs the timeline. Need for all write requests.
+#https://www.rememberthemilk.com/services/api/timelines.rtm
 get_timeline () {
     method="rtm.timelines.create"
     args="method=$method&$standard_args"
@@ -71,17 +91,15 @@ get_timeline () {
     echo "timeline=$timeline" >> .rtmcfg
 }
 
-#api list methods
-. ~/.rtmcfg
-
-#lists
+#Gets a list of lists.
+#https://www.rememberthemilk.com/services/api/methods/rtm.lists.getList.rtm
 lists_getList () {
     method="rtm.lists.getList"
     args="method=$method&$standard_args"
     sig=$(get_sig "$args")
     $wget_cmd "$api_url?$args&api_sig=$sig" > /tmp/lists.json
 }
-
+#This matches the list name with its ID.
 index_lists () {
 > /tmp/list-vars.txt
 c=0
@@ -95,7 +113,8 @@ c=0
     done 
 }
 
-#tasks
+#Grab the tasks and save the json to tmp
+#https://www.rememberthemilk.com/services/api/methods/rtm.tasks.getList.rtm
 tasks_getList () {
 . /tmp/list-vars.txt
     method="rtm.tasks.getList"
@@ -103,7 +122,8 @@ tasks_getList () {
     sig=$(get_sig "$args")
     $wget_cmd "$api_url?$args&api_sig=$sig" > /tmp/tasks.json
 }
-
+#This grabs the useful data from the json file and
+#converts it to a csv.
 index_tasks () {
 > /tmp/tasks.csv
 c=0
@@ -128,14 +148,24 @@ c=0
     c=$((c+1))
     done
 }
+#Bundle the above four steps for one sync.
+sync_tasks () {
+    lists_getList
+    index_lists
+    tasks_getList
+    index_tasks
+}
+#this is the default sorting order. First by priority,
+#then by due date.
 sort_priority () {
-    sort -t',' -k1 /tmp/tasks.csv > /tmp/by-priority.csv
+    sort -t',' -k1,2 /tmp/tasks.csv > /tmp/by-priority.csv
 }
-
+#you can get it sorted by date if you prefer.
 sort_date () {
-    sort -t',' -k2 /tmp/tasks.csv > /tmp/by-date.csv
+    sort -t',' -k2,1 /tmp/tasks.csv > /tmp/by-date.csv
 }
-
+#this creates an 'item' array, so that we can pick the
+#exact task we need to complete.
 index_csv () {
 > /tmp/indexed_tasks.csv
 d=1
@@ -145,6 +175,8 @@ d=1
     d=$((d+1))
     done < $1
 }
+#this displays your tasks to stdout looking reasonably,
+#I think. I want to add colour.
 display_tasks () {
 c=1
     index_csv $1
@@ -161,7 +193,9 @@ c=1
     done < /tmp/indexed_tasks.csv
 }
 
-#complete task
+#This will mark a task as complete. And this action can
+#be undone if you need.
+#https://www.rememberthemilk.com/services/api/methods/rtm.tasks.complete.rtm
 tasks_complete () {
     method="rtm.tasks.complete"
     x=$(grep "item\[$1\]" /tmp/indexed_tasks.csv | sed "s/item\[$1\]=//g" )
@@ -178,7 +212,10 @@ tasks_complete () {
         echo "something bad hapon"
     fi
 }
-
+#Add a task. For the sake of... simplicity, its usually
+#best to always add to a specific list. Something wonky
+#happens atm if there are tasks in the Inbox.
+#https://www.rememberthemilk.com/services/api/methods/rtm.tasks.add.rtm
 tasks_add () {
     method="rtm.tasks.add"
     l_id=$(echo "$list_name" | xargs -I{} grep {} /tmp/list-vars.txt | cut -d'=' -f2)
@@ -192,19 +229,39 @@ tasks_add () {
         echo "something bad hapon"
     fi
 }
-#lists_getList
-#tasks_getList
-#index_tasks
-#index_csv /tmp/tasks.csv
-#display_tasks /tmp/tasks.csv
-#tasks_complete $1
-#task_add "$2"
 
+#does the actions below. i should add a 'help' section.
+#Note that it syncs your tasks everytime you add or 
+#complete one.
 for i in "$@"
 do
 case $i in
+    list)
+        sync_tasks
+        sort_priority
+        display_tasks /tmp/by-priority.csv
+    shift
+    ;;
     add)
         tasks_add "$2"
+    shift
+    ;;
+    complete)
+        tasks_complete "$2"
+    shift
+    ;;
+    sync)
+        sync_tasks
+    shift
+    ;;
+    authorize)
+        authenticate
+        . ~/.rtmcfg
+    shift
+    ;;
+    -d)
+        sort_date
+        display_tasks /tmp/by-date.csv
     shift
     ;;
 esac
