6d51917170382e208ca013f3c9e40e0f0950c850
b. binaohan <b@binaohan.org>
2017-12-20 08:20:15 -0500

    test webhook

--- rtm-cli.sh
+++ rtm-cli.sh
@@ -184,7 +184,7 @@ c=1
 
 #does the actions below. i should add a 'help' section.
 #Note that it syncs your tasks everytime you add or 
-#complete one. sdsdassdsdfsfsdfsdfsdf
+#complete one. sdsdassdsdfsfsdfsdfsdfsdf
 for i in "$@"
 do
 case $i in
